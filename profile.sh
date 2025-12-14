f457fe73190dd407b5bf1e2d7194b58bb6588488
Aleksei <aleksei.kotliarov@epita.fr>
2025-12-14 15:24:36 +0100

    nsys summary example

--- profile.sh
+++ profile.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+stream="./build/stream"
+if [ "$#" -eq 3 ]; then
+    stream="$3"
+fi
+
 tag=$2
 if [ "$1" = "nsys" ]; then
-    echo "nsys $tag"
+    nsys profile \
+        --trace=cuda,cublas,osrt,nvtx \
+        --stats=true \
+        --output="$tag" \
+        $stream --mode=gpu samples/ACETx4.mp4 --output=test.mp4
 else
-    echo "ncu $tag"
+    sudo ncu \
+        --set full \
+        -o "$tag" \
+        $stream --mode=gpu samples/ACETx4.mp4 --output=test.mp4
 fi
